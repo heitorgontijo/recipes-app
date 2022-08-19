14a6c58dd3a72d34543ea5fab8ab483f0390b37e
Heitor Gontijo <heitor.tiburcio9@gmail.com>
2022-08-19 17:38:26 -0300

    Exercicio 5

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Login from '../pages/Login'
+import userEvent from '@testing-library/user-event'
+// import renderWithRouter from '../tests/helpers/renderWithRouter'
 
 describe('Testa componente Login', () => {
 it('Testa se input de e-mail está sendo renderizado', () => {
@@ -19,6 +21,20 @@ it('Testa se o botão está sendo renderizado', () => {
     render(<Login />)
     const login = screen.getByTestId('login-submit-btn')
     expect(login).toBeInTheDocument()
+
+})
+it('Testa se o botão redireciona para /foods', () => {
+    render(<Login />)
+    const input = screen.getByTestId('email-input')
+    const password = screen.getByTestId('password-input')
+
+    userEvent.type(input, 'email@test.com');
+    userEvent.type(password, '12345678');
+    const btn = screen.getByTestId('login-submit-btn')
+    expect(btn).toBeEnabled();
+    // userEvent.click(btn);
+    // expect()
+    
 })
 
 })
