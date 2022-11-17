3b3805f0fbb749bdaf0f357ecc2fa7775733c54d
Heitor Gontijo <heitor.tiburcio9@gmail.com>
2022-11-17 18:43:32 -0300

    corrigindo complexidade

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -9,6 +9,29 @@ import setDoneRecipe from '../storage/setDoneRecipe';
 import FavoriteAndShare from '../components/FavoriteAndShare';
 import * as Styled from './RecipeInProgress.styles';
 
+const finishRecipe = (history) => {
+  const recipeDetailsToStore = {
+    id,
+    type: RECIPE_TYPE === 'meals' ? 'food' : 'drink',
+    nationality: recipe.strArea || '',
+    category: recipe.strCategory || '',
+    alcoholicOrNot: recipe.strAlcoholic || '',
+    name: recipe.strMeal || recipe.strDrink,
+    image: recipe.strMealThumb || recipe.strDrinkThumb,
+    doneDate: new Date().toLocaleDateString(),
+    tags: recipe.strTags?.split(',') || [],
+  };
+
+  setDoneRecipe(recipeDetailsToStore);
+  history.push('/done-recipes');
+};
+
+const updateCheckedItems = (setIngredientsChecked) => {
+  const storedRecipesInProgress = getStorage(
+    'inProgressRecipes', { cocktails: {}, meals: {} },
+  );
+  setIngredientsChecked(storedRecipesInProgress[RECIPE_IDENTIFIER][id] || []);
+};
 function RecipeInProgress() {
   const history = useHistory();
   const { location: { pathname } } = history;
@@ -21,12 +44,7 @@ function RecipeInProgress() {
   const RECIPE_TYPE = pathname.match(/foods\//i) ? 'meals' : 'drinks';
   const RECIPE_IDENTIFIER = RECIPE_TYPE === 'meals' ? 'meals' : 'cocktails';
 
-  const updateCheckedItems = () => {
-    const storedRecipesInProgress = getStorage(
-      'inProgressRecipes', { cocktails: {}, meals: {} },
-    );
-    setIngredientsChecked(storedRecipesInProgress[RECIPE_IDENTIFIER][id] || []);
-  };
+  updateCheckedItems(setIngredientsChecked);
 
   useEffect(() => {
     fetchRecipeDetails(id, RECIPE_TYPE)
@@ -52,22 +70,7 @@ function RecipeInProgress() {
     setInProgress(id, ingredient, RECIPE_IDENTIFIER); updateCheckedItems();
   };
 
-  const finishRecipe = () => {
-    const recipeDetailsToStore = {
-      id,
-      type: RECIPE_TYPE === 'meals' ? 'food' : 'drink',
-      nationality: recipe.strArea || '',
-      category: recipe.strCategory || '',
-      alcoholicOrNot: recipe.strAlcoholic || '',
-      name: recipe.strMeal || recipe.strDrink,
-      image: recipe.strMealThumb || recipe.strDrinkThumb,
-      doneDate: new Date().toLocaleDateString(),
-      tags: recipe.strTags?.split(',') || [],
-    };
-
-    setDoneRecipe(recipeDetailsToStore);
-    history.push('/done-recipes');
-  };
+  finishRecipe(history);
 
   return (
     <Styled.RecipeInProgress>
